027c4c5545226ac27a3f86afe8974b719ac96cb5
Micah <micah@whamdonk.com>
2014-03-07 16:38:16 -0600

    Removed the unneeded files

--- public/js/modules/pages/collections/pages.js
+++
@@ -1,8 +0,0 @@
-define(['backbone', 'modules/pages/models/page'], function (Backbone, PageModel) {
-    var PageCollection = Backbone.Collection.extend({
-        url:'/api/pagebuilder/collection',
-        model:PageModel
-    });
-
-    return PageCollection;
-});
--- public/js/modules/pages/models/page.js
+++
@@ -1,7 +0,0 @@
-define(['backbone'], function (Backbone) {
-    var PageModel = Backbone.Model.extend({
-        defaults:{slug:'asfd', content:'content'} 
-    });
-
-    return PageModel;
-});
