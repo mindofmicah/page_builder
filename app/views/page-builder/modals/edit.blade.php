d072725f713cf1f48a23254d2881c089006fe058
Micah <micah@whamdonk.com>
2014-03-11 12:18:13 -0500

    Added styling + Save button

--- app/views/page-builder/modals/edit.blade.php
+++ app/views/page-builder/modals/edit.blade.php
@@ -31,7 +31,8 @@
 
                             </div>
                             <div class="modal-footer">
-                            <button type="button" data-dismiss=".modal">Close</button>
+                            <button type="button" class="btn btn-default" data-dismiss=".modal">Close</button>
+                            <button type="submit" class="btn btn-primary">Save Changes</button>
                             </div>
                             </form>
                     </div>
