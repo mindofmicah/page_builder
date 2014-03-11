a42e56d8a80116fcf41e1faa2da60137efa8786a
Micah <micah@whamdonk.com>
2014-03-11 12:22:10 -0500

    Fixed the content field to the proper id

--- public/js/modules/pages/views/modal.js
+++ public/js/modules/pages/views/modal.js
@@ -8,9 +8,7 @@ define(['backbone', 'bootstrap'], function (Backbone, Bootstrap) {
         this.render();
              this.fields.slug = this.$el.find('#slug');
             this.fields.type = this.$el.find('#type');
-            this.fields.content = this.$el.find('#content');
-console.log(this.fields);
-console.log(this.$el);
+            this.fields.content = this.$el.find('#page-content');
      
         },
         events: {
@@ -25,9 +23,7 @@ console.log(this.$el);
             for(var key in this.fields) {
                 ret[key] = this.fields[key].val();
             }
-            console.log(ret);
             this.model.set(ret);
-            console.log(this.model);
         },
         render : function () {
             this.$el.html(this.template(this.model.toJSON()));
